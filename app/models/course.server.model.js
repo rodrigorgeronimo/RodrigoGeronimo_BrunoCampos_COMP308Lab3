a26f9c696db756c3c9c084b3ad1f4273ef89760a
Rodrigo Geronimo <rgeronim@my.centennialcollege.ca>
2018-03-29 14:42:42 -0400

    Back End Structure

---
+++ app/models/course.server.model.js
@@ -0,0 +1,26 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CourseSchema = new Schema({
+    courseCode: {
+        type: String,
+        trim: true,
+        required: 'Course Code cannot be blank'
+    },
+    courseName: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Course Name cannot be blank'
+    },
+    section: {
+        type: String, default: '',
+        trim: true,
+        required: 'Section cannot be blank'
+    },
+    semester: {
+        type: String, default: '',
+        trim: true,
+        required: 'Semester cannot be blank'
+    }
+});
+mongoose.model('Course', CourseSchema);
---
+++ app/models/student.server.model.js
@@ -0,0 +1,114 @@
+﻿// Load the module dependencies
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const Schema = mongoose.Schema;
+
+// Define a new 'StudentSchema'
+const StudentSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        // Validate the email format
+        match: [/.+\@.+\..+/, "Please fill a valid email address"]
+    },
+    studentNumber: {
+        type: String,
+        // Set a unique 'studentNumber' index
+        unique: true,
+        // Validate 'studentNumber' value existance
+        required: 'Student number is required',
+        // Trim the 'studentNumber' field
+        trim: true
+    },
+    password: {
+        type: String,
+        required: 'Password is required',
+        // Validate the 'password' value length
+        validate: [
+            (password) => password && password.length > 6,
+            'Password should be longer'
+        ]
+    },
+
+    address: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    postalCode: {
+        type: String,
+        validate: [
+            (postalCode) => postalCode && postalCode.length <= 6,
+            'Postal Code should be 6 characters'
+        ]
+    },
+    phoneNumber: {
+        type: String
+    },
+    program: {
+        type: String
+    },
+
+
+
+    salt: {
+        type: String
+    },
+    provider: {
+        type: String,
+        // Validate 'provider' value existance
+        required: 'Provider is required'
+    },
+    providerId: String,
+    providerData: {},
+    created: {
+        type: Date,
+        // Create a default 'created' value
+        default: Date.now
+    }
+
+    //a field to indicate the courses student is taking
+
+    //an array of course objects
+});
+
+// Set the 'fullname' virtual property
+StudentSchema.virtual('fullName').get(function () {
+    return this.firstName + ' ' + this.lastName;
+}).set(function (fullName) {
+    const splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
+});
+
+// Use a pre-save middleware to hash the password
+StudentSchema.pre('save', function (next) {
+    if (this.password) {
+        this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.password = this.hashPassword(this.password);
+    }
+
+    next();
+});
+
+// Create an instance method for hashing a password
+StudentSchema.methods.hashPassword = function (password) {
+    //digest parameter required in version 9 of Node.js
+    return crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha512').toString('base64');
+};
+
+// Create an instance method for authenticating user
+StudentSchema.methods.authenticate = function (password) {
+    return this.password === this.hashPassword(password);
+};
+
+// Configure the 'UserSchema' to use getters and virtuals when transforming to JSON
+StudentSchema.set('toJSON', {
+    getters: true,
+    virtuals: true
+});
+
+// Create the 'User' model out of the 'UserSchema'
+mongoose.model('Student', StudentSchema);
